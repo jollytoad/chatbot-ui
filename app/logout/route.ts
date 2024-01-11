8fb0bfa460ee873cf89ec60ae4d4d95d95fee318
Mark Gibson <mgibson@adaptavist.com>
2024-01-11 14:21:56 +0000

    Add logout route to allow a forced logout

---
+++ app/logout/route.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@/lib/supabase/server"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+export async function GET(request: Request) {
+  try {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    await supabase.auth.signOut()
+  } catch {}
+
+  return redirect("/")
+}
