c758de0c11717a0f4598d825c41ccec067313c87
Mark Gibson <mgibson@adaptavist.com>
2024-02-02 13:00:13 +0000

    Squash org branch over latest main, without some of the more conflicting changes.

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { createClient } from "@/lib/supabase/middleware"
 import { i18nRouter } from "next-i18n-router"
 import { NextResponse, type NextRequest } from "next/server"
 import i18nConfig from "./i18nConfig"
+import { publicUrl } from '@/lib/public-url'
 
 export async function middleware(request: NextRequest) {
   const i18nResult = i18nRouter(request, i18nConfig)
@@ -10,12 +11,12 @@ export async function middleware(request: NextRequest) {
   try {
     const { supabase, response } = createClient(request)
 
-    const session = await supabase.auth.getSession()
+    const { data: { session } } = await supabase.auth.getSession()
 
     const redirectToChat = session && request.nextUrl.pathname === "/"
 
     if (redirectToChat) {
-      return NextResponse.redirect(new URL("/chat", request.url))
+      return NextResponse.redirect(new URL("/chat", publicUrl(request)))
     }
 
     return response
